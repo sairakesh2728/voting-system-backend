2cab2cfef54fc9b04e3b8b688e422f4799188f95
sairakesh2728 <161056378+sairakesh2728@users.noreply.github.com>
2026-07-12 21:03:27 +0530

    Change mail configuration to use SSL

--- main.py
+++ main.py
@@ -51,10 +51,10 @@ mail_conf = ConnectionConfig(
     MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
     MAIL_FROM = os.getenv("MAIL_USERNAME"),
-    MAIL_PORT = 587,
+    MAIL_PORT = 465,
     MAIL_SERVER = "smtp.gmail.com",
-    MAIL_STARTTLS = True,
-    MAIL_SSL_TLS = False,
+    MAIL_STARTTLS = False,
+    MAIL_SSL_TLS = True,
     USE_CREDENTIALS = True,
     VALIDATE_CERTS = True
 )
