ff4613d00ee0213ddc05c1b6ee067a25a0e743ed
sairakesh2728 <161056378+sairakesh2728@users.noreply.github.com>
2026-07-13 22:17:13 +0530

    Update mail configuration for SMTP settings

--- main.py
+++ main.py
@@ -51,10 +51,10 @@ mail_conf = ConnectionConfig(
     MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
     MAIL_FROM = os.getenv("MAIL_USERNAME"),
-    MAIL_PORT = 465,
-    MAIL_SERVER = "smtp.gmail.com",
-    MAIL_STARTTLS = False,
-    MAIL_SSL_TLS = True,
+    MAIL_PORT = 587,
+    MAIL_SERVER = "smtp-relay.brevo.com",
+    MAIL_STARTTLS = True,
+    MAIL_SSL_TLS = False,
     USE_CREDENTIALS = True,
     VALIDATE_CERTS = True
 )
